d5b53967a01acacc794b77e2cf7aed655f647aa6
rari <suffocationanymore@gmail.com>
2021-07-15 11:36:04 +0300

    Ошибки линтеров

--- build/js/script.js
+++ build/js/script.js
@@ -4,18 +4,22 @@ const nav = document.querySelector('.nav');
 const toggler = document.querySelector('.header__toggler');
 const logo = document.querySelector('.header__logo');
 
-toggler.classList.remove('header__toggler--close');
-nav.classList.add('nav--closed');
-logo.classList.add('header__logo--show');
+const toggleNavOnClick = () => {
+  toggler.classList.remove('header__toggler--close');
+  nav.classList.add('nav--closed');
+  logo.classList.add('header__logo--show');
 
-toggler.addEventListener('click', () => {
-  if (nav.classList.contains('nav--closed')) {
-    nav.classList.remove('nav--closed');
-    toggler.classList.add('header__toggler--close');
-    logo.classList.remove('header__logo--show');
-  } else {
-    nav.classList.add('nav--closed');
-    toggler.classList.remove('header__toggler--close');
-    logo.classList.add('header__logo--show');
-  }
-})
+  toggler.addEventListener('click', () => {
+    if (nav.classList.contains('nav--closed')) {
+      nav.classList.remove('nav--closed');
+      toggler.classList.add('header__toggler--close');
+      logo.classList.remove('header__logo--show');
+    } else {
+      nav.classList.add('nav--closed');
+      toggler.classList.remove('header__toggler--close');
+      logo.classList.add('header__logo--show');
+    }
+  })
+}
+
+toggleNavOnClick();
